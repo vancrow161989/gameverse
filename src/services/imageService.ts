823407de67db78ea1a36e87057d07da4ee0e523d
vancrow16 <vancrow161989@gmail.com>
2023-07-25 14:44:56 +0800

    Updated No Image

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -1,5 +1,5 @@
 export function getCropImage(imageUrl: string) {
-  if (!imageUrl) return "https://placehold.co/600x400";
+  if (!imageUrl) return "https://placehold.co/600x400?text=No+Image";
   const target = "media/";
   const index = imageUrl.indexOf(target) + target.length;
   return imageUrl.slice(0, index) + `crop/600/400/` + imageUrl.slice(index);
