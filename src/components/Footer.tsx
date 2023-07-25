5152887ae302a044d22e7aba0faf775eebaa7df4
vancrow16 <vancrow161989@gmail.com>
2023-07-25 18:13:57 +0800

    adjusted footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,10 @@ import { Box, Text } from "@chakra-ui/react";
 
 function Footer() {
   return (
-    <Box textAlign="right" padding={9} marginTop={5}>
+    <Box
+      textAlign={{ base: "center", lg: "right" }}
+      padding={{ base: 5, md: 9 }}
+      marginTop={{ md: 5 }}>
       <Text>© 2023 Michael Ivan Togeno. All Rights Reserved.</Text>
     </Box>
   );
