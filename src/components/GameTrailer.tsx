2190cefedf11b0ae1e47efe69399708930180efe
vancrow16 <vancrow161989@gmail.com>
2023-08-02 21:51:01 +0800

    fixed issue throws error if no game trailer

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -9,11 +9,12 @@ function GameTrailer({ gameId }: Props) {
   const { data: gameTrailer, error, isLoading } = useGameTrailers(gameId);
 
   if (isLoading) return <Spinner></Spinner>;
+  if (gameTrailer?.results && gameTrailer.results.length <= 0) return null;
   if (error || !gameTrailer) throw error;
   const first = gameTrailer.results[0];
   return (
     <Box>
-      <video src={first.data.max} poster={first.preview} controls></video>
+      <video src={first?.data.max} poster={first?.preview} controls></video>
     </Box>
   );
 }
